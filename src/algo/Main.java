79cc003b2d57b26daf911ae7ca576924e68c54ed
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2014-12-21 21:35:43 +0300

    Move the test data to the method main. TODO: write a test

--- src/algo/Main.java
+++ src/algo/Main.java
@@ -6,24 +6,6 @@ import java.util.Queue;
 public class Main {
 
     public static void main(String[] args) {
-        printTree();
-    }
-
-    /*
-     *           a
-     *          /  \
-     *         b    c
-     *        / \   /\
-     *       d  e  f  g
-     *      /\        /\
-     *     h  i       l m
-     */
-    /**
-     * A binary tree represent an organization hierarchy. 
-     * The root node is the CEO and etc. design a algorithm to print the tree level by level.
-     * TODO: start a new line when starting a new level
-     */
-    static void printTree() {
         Node l = new Node(null, null, "l");
         Node m = new Node(null, null, "m");
         Node n = new Node(null, null, "n");
@@ -41,6 +23,24 @@ public class Main {
         Node c = new Node(f, g, "c");
 
         Node root = new Node(b, c, "root");
+        printTree(root);
+    }
+
+    /*
+     *           a
+     *          /  \
+     *         b    c
+     *        / \   /\
+     *       d  e  f  g
+     *      /\        /\
+     *     h  i       l m
+     */
+    /**
+     * A binary tree represent an organization hierarchy.
+     * The root node is the CEO and etc. design a algorithm to print the tree level by level.
+     * TODO: start a new line when starting a new level
+     */
+    static void printTree(Node root) {
         Queue<Node> nodes = new LinkedList<Node>();
         nodes.add(root);
         while (!nodes.isEmpty()) {
